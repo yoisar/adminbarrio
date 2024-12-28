2879e14b149165e8479c776c017fb77464ed4c14
Yassel Izquierdo <sioy23@gmail.com>
2024-12-28 10:02:42 -0300

    Add laravel vite plugin and laravel basic ui login and register

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Models\User;
+use App\Notifications\TestNotification;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/send-notification', function () {
+    $user = User::first(); // Obtén el primer usuario de la base de datos
+    $user->notify(new TestNotification()); // Enviar la notificación
+
+    return 'Notificación enviada';
+});
+
+// Auth::routes();
+
+// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
