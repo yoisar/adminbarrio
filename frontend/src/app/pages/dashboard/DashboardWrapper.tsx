79272ec4b324031a581c7f7d5822f92063ff9cff
Yassel Izquierdo <sioy23@gmail.com>
2025-01-23 16:18:52 -0300

    hide dashboard widgets not used at the moment

--- frontend/src/app/pages/dashboard/DashboardWrapper.tsx
+++ frontend/src/app/pages/dashboard/DashboardWrapper.tsx
@@ -4,17 +4,6 @@ import { toAbsoluteUrl } from '../../../_metronic/helpers'
 import { Content } from '../../../_metronic/layout/components/content'
 import { ToolbarWrapper } from '../../../_metronic/layout/components/toolbar'
 import { PageTitle } from '../../../_metronic/layout/core'
-import {
-    EngageWidget10,
-    ListsWidget2,
-    ListsWidget26,
-    ListsWidget3,
-    ListsWidget4,
-    ListsWidget6,
-    MixedWidget8,
-    TablesWidget10,
-    TablesWidget5
-} from '../../../_metronic/partials/widgets'
 import { DashboardWidget } from '../../modules/adminbarrio/components/dashboard/DashboardWidget'
 import { PaymentsWidget } from '../../modules/adminbarrio/components/dashboard/PaymentsWidget'
 import { UsersWidget } from '../../modules/adminbarrio/components/UsersWidget'
@@ -51,13 +40,13 @@ const DashboardPage: FC = () => (
         {/* <Dashboard 
         // className='h-md-50 mb-5 mb-xl-10' 
         /> */}
-        <ListsWidget26 className='h-lg-50' />
+        {/* <ListsWidget26 className='h-lg-50' /> */}
       </div>
       {/* end::Col */}
 
       {/* begin::Col */}
       <div className='col-xxl-6'>
-        <EngageWidget10 className='h-md-100' />
+        {/* <EngageWidget10 className='h-md-100' /> */}
       </div>
       {/* end::Col */}
     </div>
@@ -82,10 +71,10 @@ const DashboardPage: FC = () => (
     {/* begin::Row */}
     <div className='row gy-5 gx-xl-8'>
       <div className='col-xxl-4'>
-        <ListsWidget3 className='card-xxl-stretch mb-xl-3' />
+        {/* <ListsWidget3 className='card-xxl-stretch mb-xl-3' /> */}
       </div>
       <div className='col-xl-8'>
-        <TablesWidget10 className='card-xxl-stretch mb-5 mb-xl-8' />
+        {/* <TablesWidget10 className='card-xxl-stretch mb-5 mb-xl-8' /> */}
       </div>
     </div>
     {/* end::Row */}
@@ -93,13 +82,13 @@ const DashboardPage: FC = () => (
     {/* begin::Row */}
     <div className='row gy-5 g-xl-8'>
       <div className='col-xl-4'>
-        <ListsWidget2 className='card-xl-stretch mb-xl-8' />
+        {/* <ListsWidget2 className='card-xl-stretch mb-xl-8' /> */}
       </div>
       <div className='col-xl-4'>
-        <ListsWidget6 className='card-xl-stretch mb-xl-8' />
+        {/* <ListsWidget6 className='card-xl-stretch mb-xl-8' /> */}
       </div>
       <div className='col-xl-4'>
-        <ListsWidget4 className='card-xl-stretch mb-5 mb-xl-8' items={5} />
+        {/* <ListsWidget4 className='card-xl-stretch mb-5 mb-xl-8' items={5} /> */}
         {/* partials/widgets/lists/_widget-4', 'class' => 'card-xl-stretch mb-5 mb-xl-8', 'items' => '5' */}
       </div>
     </div>
@@ -107,14 +96,14 @@ const DashboardPage: FC = () => (
 
     <div className='row g-5 gx-xxl-8'>
       <div className='col-xxl-4'>
-        <MixedWidget8
+        {/* <MixedWidget8
           className='card-xxl-stretch mb-xl-3'
           chartColor='success'
           chartHeight='150px'
-        />
+        /> */}
       </div>
       <div className='col-xxl-8'>
-        <TablesWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' />
+        {/* <TablesWidget5 className='card-xxl-stretch mb-5 mb-xxl-8' /> */}
       </div>
     </div>
     </Content>
